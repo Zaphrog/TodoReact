c42c9da7f41c7f74837cefaed42b348823474fbd
Zaphrog <diegocottin@gmail.com>
2022-02-22 19:02:41 +0000

    first

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,37 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+function TodoList(props) {
+	console.log("HEEELLLLLOOOOOO");
+	const [todo, setTodo] = useState([]);
+	const [task, setTask] = useState("");
+	function addTask() {
+		if (task != "") {
+			setTodo([...todo, task]);
+			setTask("");
+		}
+	}
+	return (
+		<div>
+			<input
+				type="text"
+				onChange={(e) => setTask(e.target.value)}
+				value={task}
+				placeholder="Please type your name"
+			/>
+			<button onClick={() => addTask()}>Add task</button>
+			<ul>
+				{todo.length === 0 && <li>No tasks</li>}
+				{todo.length != 0 && todo.map((t) => <li>{t}</li>)}
+			</ul>
+		</div>
+	);
+}
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TodoList />, document.querySelector("#app"));
